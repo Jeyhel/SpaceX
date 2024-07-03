4cb48b87996cdcdaf19443370e248dca84d4b16a
jeyhel <jeyhelangelina0212@gmail.com>
2024-07-03 09:08:47 -0500

    feat: :rocket: se agrego style de css

--- js/components/plantiCompany.js
+++ js/components/plantiCompany.js
@@ -8,22 +8,22 @@ export const InfoCompany = async (plusInfo) => {
     document.querySelector(".nav__description").innerHTML = /*html*/`
     <div class="company1">
 
-        <h3> founder</h3> 
-        <p> ${plusInfo.founder} </p>
-
-        <h3> founded</h3> 
+        <h3> Founder</h3> 
+        <p> ${plusInfo.founder} </p>    
+        <hr>
+        <h3> Founded</h3> 
         <p> ${plusInfo.founded} </p>
-
-        <h3> employees</h3> 
+        <hr>
+        <h3> Employees</h3> 
         <p> ${plusInfo.employees} </p>
-
-        <h3> vehicles</h3> 
+        <hr>
+        <h3> Vehicles</h3> 
         <p> ${plusInfo.vehicles} </p>
-
-        <h3> launch_sites</h3> 
+        <hr>
+        <h3> Launch_sites</h3> 
         <p> ${plusInfo.launch_sites} </p>
-
-        <h3> test_sites</h3>
+        <hr>
+        <h3> Test_sites</h3>
         <p> ${plusInfo.test_sites} </p>
  
     
@@ -34,14 +34,14 @@ export const InfoCompany = async (plusInfo) => {
     <div class="company2">
     
 
-        <h3>address</h3>
+        <h3>Address</h3>
         <p> ${plusInfo.headquarters.address} </p>
         <br>
     
-        <h3>city</h3>
+        <h3>City</h3>
         <p> ${plusInfo.headquarters.city} </p>
         <br>
-        <h3>state </h3>
+        <h3>State </h3>
         <p> ${plusInfo.headquarters.state} </p>
         <br>
     
@@ -52,15 +52,15 @@ export const InfoCompany = async (plusInfo) => {
     document.querySelector(".sucess_rate_rocket").innerHTML = /*html*/`
     <div class="company3">
 
-    <h3> website</h3>
+    <h3> Website</h3>
         <a href ="${plusInfo.links.website}"> 
         <p> Read me..</p>
         </a>
-    <h3> twitter</h3>
+    <h3> Twitter</h3>
         <a href ="${plusInfo.links.twitter}"> 
         <p> Read me..</p>
         </a>
-    <h3> elon_twitter</h3>
+    <h3> Elon_twitter</h3>
         <a href ="${plusInfo.links.elon_twitter}"> 
         <p> Read me..</p>
         </a>
@@ -72,19 +72,25 @@ export const InfoCompany = async (plusInfo) => {
     document.querySelector(".information__item").innerHTML = /*html*/`
     <div class="company4">
     
-        <h3>ceo</h3>
+        <h3>Ceo</h3>
         <p> ${plusInfo.ceo} </p>
-        <h3>cto</h3>
+        <hr>
+        <h3>Cto</h3>
         <p> ${plusInfo.cto} </p>
-        <h3>coo</h3>
+        <hr>
+        <h3>Coo</h3>
         <p> ${plusInfo.coo} </p>
-        <h3>cto_propulsion</h3>
+        <hr>
+        <h3>Cto_propulsion</h3>
         <p> ${plusInfo.cto_propulsion} </p>
-        <h3>valuation</h3>
+        <hr>
+        <h3>Valuation</h3>
         <p> ${plusInfo.valuation} </p>
-        <h3>summary</h3>
+        <hr>
+        <h3>Summary</h3>
         <p> ${plusInfo.summary} </p>
-        <h3>id</h3>
+        <hr>
+        <h3>Id</h3>
         <p> ${plusInfo.id} </p>
     
     </div>
